7a028b1172993de7769eff9d28e11fdb9e1ceff5
matt-quant-heads-io <77163184+matt-quant-heads-io@users.noreply.github.com>
2023-11-27 16:00:54 -0500

    Update env.sh

--- env.sh
+++ env.sh
@@ -5,4 +5,4 @@ export PROJECT_ROOT="/scratch/${USER}/overlay/sweep_testing_pod"
 export ZELDA_DATA_ROOT="${PROJECT_ROOT}/data/zelda"
 export ZELDA_GOAL_MAPS_ROOT="${PROJECT_ROOT}/goal_maps/zelda"
 
-alias python=`${SCRATCH}/miniconda3/envs/sweep_testing_pod/bin/python`
+alias python=${SCRATCH}/overlay/miniconda3/envs/sweep_testing_pod/bin/python
