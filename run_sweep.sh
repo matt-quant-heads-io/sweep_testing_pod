c0367c8fb8b154c5cbf6ca5e6ef9ae1b9d582475
matt-quant-heads-io <77163184+matt-quant-heads-io@users.noreply.github.com>
2023-11-27 15:57:45 -0500

    Update run_sweep.sh

--- run_sweep.sh
+++ run_sweep.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-source /scratch/${USER}/overlay/env.sh
+source ${SCRATCH}/overlay/sweep_testing_pod/env.sh
 conda activate sweep_testing_pod
 
-source /scratch/${USER}/overlay/sweep_testing_pod/env.sh
 cd ${PROJECT_ROOT}
-
 
 # TO run 'gen_train_data' process uncomment below
 python main.py experiments=exp_1 +mode=controllable +username=ms12010 +domain=zelda +process=gen_train_data
